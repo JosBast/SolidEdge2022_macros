c194533a588168dc23911726570fa512fad8cce2
Jose <maksselemin@gmail.com>
2024-01-08 22:44:46 +0300

    Добавьте файлы проекта.

---
+++ Kursach/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Kursach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textVal1 = new System.Windows.Forms.TextBox();
+            this.textName1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textVal2 = new System.Windows.Forms.TextBox();
+            this.textName2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textVal3 = new System.Windows.Forms.TextBox();
+            this.textName3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(411, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(350, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Изменить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textVal1
+            // 
+            this.textVal1.Location = new System.Drawing.Point(386, 64);
+            this.textVal1.Name = "textVal1";
+            this.textVal1.Size = new System.Drawing.Size(375, 20);
+            this.textVal1.TabIndex = 1;
+            this.textVal1.TextChanged += new System.EventHandler(this.textName1_TextChanged);
+            // 
+            // textName1
+            // 
+            this.textName1.Location = new System.Drawing.Point(5, 64);
+            this.textName1.Name = "textName1";
+            this.textName1.Size = new System.Drawing.Size(375, 20);
+            this.textName1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(232)))));
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.textVal2);
+            this.panel1.Controls.Add(this.textName2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.textVal3);
+            this.panel1.Controls.Add(this.textName3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textVal1);
+            this.panel1.Controls.Add(this.textName1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(764, 403);
+            this.panel1.TabIndex = 3;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Lucida Sans", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label10.Location = new System.Drawing.Point(388, 116);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(360, 22);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Введите значение перменной (var)\r\n";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Lucida Sans", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label9.Location = new System.Drawing.Point(5, 116);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(305, 22);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Введите имя перменной (var)\r\n";
+            // 
+            // textVal2
+            // 
+            this.textVal2.Location = new System.Drawing.Point(388, 141);
+            this.textVal2.Name = "textVal2";
+            this.textVal2.Size = new System.Drawing.Size(375, 20);
+            this.textVal2.TabIndex = 15;
+            // 
+            // textName2
+            // 
+            this.textName2.Location = new System.Drawing.Point(3, 141);
+            this.textName2.Name = "textName2";
+            this.textName2.Size = new System.Drawing.Size(375, 20);
+            this.textName2.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(409, 351);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(350, 29);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Создать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Lucida Sans", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label8.Location = new System.Drawing.Point(585, 319);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(149, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "=(Ваша формула)\r\n";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Lucida Sans", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label7.Location = new System.Drawing.Point(384, 271);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(311, 22);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Введите формулу перменной ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label6.Location = new System.Drawing.Point(5, 271);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(254, 22);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Введите имя перменной";
+            // 
+            // textVal3
+            // 
+            this.textVal3.Location = new System.Drawing.Point(386, 296);
+            this.textVal3.Name = "textVal3";
+            this.textVal3.Size = new System.Drawing.Size(375, 20);
+            this.textVal3.TabIndex = 9;
+            // 
+            // textName3
+            // 
+            this.textName3.Location = new System.Drawing.Point(5, 296);
+            this.textName3.Name = "textName3";
+            this.textName3.Size = new System.Drawing.Size(375, 20);
+            this.textName3.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(306, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Создание новой переменной\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(418, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Изменение существующей переменной\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(546, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "(В метрах, через запятую)\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(382, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(366, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите значение перменной (dim)\r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(3, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(311, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите имя перменной (dim)\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(222)))), ((int)(((byte)(222)))));
+            this.ClientSize = new System.Drawing.Size(788, 427);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Концептуальный интерфейс для SE Макса Шелемина ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textVal1;
+        private System.Windows.Forms.TextBox textName1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textVal3;
+        private System.Windows.Forms.TextBox textName3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textVal2;
+        private System.Windows.Forms.TextBox textName2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Kursach/Form1.cs
@@ -0,0 +1,216 @@
+﻿using SolidEdgeCommunity;
+using SolidEdgeFramework;
+using SolidEdgeFrameworkSupport;
+using SolidEdgePart;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Application = SolidEdgeFramework.Application;
+
+namespace Kursach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private static Application seApplication;
+
+        [STAThread]
+        static void Kurs(string name1, double val1, string name2, double val2)
+        {
+
+            SolidEdgeFramework.Application application = null;
+            SolidEdgeFramework.Documents seDocuments = null;
+
+            SolidEdgePart.PartDocument sePartDoc = null;
+
+            SolidEdgeFramework.Variables variables = null;
+            SolidEdgeFramework.VariableList listOfVariebles = null;
+            SolidEdgeFramework.VariableList listOfDimensions = null;
+
+            SolidEdgeFramework.variable varToModify = null;
+            SolidEdgeFrameworkSupport.Dimension dimToModify = null;
+            SolidEdgeFramework.variable varToCreate = null;
+
+            try
+            {
+                OleMessageFilter.Register();
+
+                seApplication = SolidEdgeUtils.Connect(true);
+
+                seDocuments = seApplication.Documents;
+
+                if (seApplication.ActiveDocumentType != SolidEdgeFramework.DocumentTypeConstants.igPartDocument)
+                {
+                    return;
+                }
+
+                sePartDoc = (PartDocument)seApplication.ActiveDocument;
+
+                variables = (Variables)sePartDoc.Variables;
+
+                listOfVariebles = (VariableList)variables.Query(
+                    pFindCriterium: "*",
+                    NamedBy: SolidEdgeConstants.VariableNameBy.seVariableNameByBoth,
+                    VarType: SolidEdgeConstants.VariableVarType.SeVariableVarTypeVariable
+                    );
+
+          ;
+
+                varToModify = (variable)listOfVariebles.Item(name2);
+                varToModify.Value = val2;
+
+                listOfDimensions = (VariableList)variables.Query(
+                    pFindCriterium: "*",
+                    NamedBy: SolidEdgeConstants.VariableNameBy.seVariableNameByBoth,
+                    VarType: SolidEdgeConstants.VariableVarType.SeVariableVarTypeDimension
+                    );
+
+
+                dimToModify = (Dimension)listOfDimensions.Item(name1);
+                dimToModify.Value = val1;
+
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                OleMessageFilter.Unregister();
+            }
+        }
+
+        public static void CrNew(string name3, string val3)
+        {
+
+            SolidEdgeFramework.Application application = null;
+            SolidEdgeFramework.Documents seDocuments = null;
+
+            SolidEdgePart.PartDocument sePartDoc = null;
+
+            SolidEdgeFramework.Variables variables = null;
+            SolidEdgeFramework.VariableList listOfVariebles = null;
+            SolidEdgeFramework.VariableList listOfDimensions = null;
+
+            SolidEdgeFramework.variable varToModify = null;
+            SolidEdgeFrameworkSupport.Dimension dimToModify = null;
+            SolidEdgeFramework.variable varToCreate = null;
+
+            try
+            {
+                OleMessageFilter.Register();
+
+                seApplication = SolidEdgeUtils.Connect(true);
+
+                seDocuments = seApplication.Documents;
+
+                if (seApplication.ActiveDocumentType != SolidEdgeFramework.DocumentTypeConstants.igPartDocument)
+                {
+                    return;
+                }
+
+                sePartDoc = (PartDocument)seApplication.ActiveDocument;
+
+                variables = (Variables)sePartDoc.Variables;
+
+
+
+                varToCreate = (variable)variables.Add(
+                    pName: name3,
+                    pFormula: val3
+                    );
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                OleMessageFilter.Unregister();
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double val1 = 0;
+            double val2 = 0;
+
+            string name1 = textName1.Text;
+            string valStr1 = textVal1.Text;
+
+            string name2 = textName2.Text;
+            string valStr2 = textVal2.Text;
+
+            if (!string.IsNullOrEmpty(valStr1) && !string.IsNullOrEmpty(name1))
+            {
+                if (!double.TryParse(valStr1, out val1))
+                {
+                    // Обработка ошибки ввода числа
+                    // Например, вывод сообщения об ошибке пользователю
+                    return;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(valStr2) && !string.IsNullOrEmpty(name2))
+            {
+                if (!double.TryParse(valStr2, out val2))
+                {
+                    // Обработка ошибки ввода числа
+                    // Например, вывод сообщения об ошибке пользователю
+                    return;
+                }
+            }
+
+            
+            Kurs(name1, val1, name2, val2);
+        }
+
+        private void textName1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            
+            string name3 = textName3.Text;
+            string val3 = textVal3.Text;
+
+            
+
+            CrNew(name3, val3);
+
+
+        }
+    }
+}
